fc64215996a5d186d82e501b25e60f4ee225c8ef
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-02 21:58:44 +0430

    add fonts

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, SafeAreaView, StatusBar, Platform} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from "expo";
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'libre-baskerville': require('./assets/fonts/libre-baskerville-regular.ttf'),
+        'libre-baskerville-bold': require('./assets/fonts/libre-baskerville-bold.ttf'),
+        'libre-baskerville-italic': require('./assets/fonts/libre-baskerville-italic.ttf')
+    });
+};
 
 const App = () => {
+    const [isLoaded, setLoaded] = useState(false);
+
+    if (!isLoaded)
+        return (
+            <AppLoading startAsync={fetchFonts} onFinish={() => setLoaded(true)}/>
+        );
+
     return (
         <SafeAreaView>
             <View style={{...styles.app, ...(Platform.OS === 'android' ? {paddingTop: StatusBar.currentHeight} : {})}}>
