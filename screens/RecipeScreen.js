e4c6142a356bf5e04d1c9bbc4f0c964a875f1781
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-03 17:58:12 +0430

    recipe screen navigation options

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
-const RecipeScreen = () => {
+import {CATEGORIES, RECIPES} from "../data/data";
+
+const RecipeScreen = ({navigation: {getParam}}) => {
+    const id = getParam('id');
+
+    const recipe = RECIPES.find(recipe => recipe.id === id);
+
     return (
         <View style={styles.screen}>
             <Text>Recipe Screen</Text>
         </View>
     );
+};
+
+RecipeScreen.navigationOptions = ({navigation: {getParam}}) => {
+    const id = getParam('id');
+
+    const recipe = RECIPES.find(recipe => recipe.id === id);
+
+    const category = CATEGORIES.find(category => category.id === recipe.categoryIds[0]);
+
+    return {
+        headerTitle: `${recipe.title} Recipes`,
+        headerStyle: {
+            backgroundColor: category.color
+        },
+        headerTintColor: '#f5f6f7'
+    };
 };
 
 const styles = StyleSheet.create({
